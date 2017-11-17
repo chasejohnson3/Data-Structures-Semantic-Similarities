268ac3a26d8f433b53a4592097ef8d3c356e81b4
Harsh Patel <harsh-patel@uiowa.edu>
2017-11-17 17:18:10 -0600

    Done with part 1

--- src/edu/uiowa/cs/similarity/Main.java
+++ src/edu/uiowa/cs/similarity/Main.java
@@ -5,6 +5,7 @@ import org.apache.commons.cli.*;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -51,13 +52,10 @@ public class Main {
             System.exit(0);
         }
         
-	
         //Scanner scanProhib = new Scanner(fileProhib);
-        //List<String> prohibWords = new ArrayList<>();
-	
-	//Thamar told us to hard code this file
-	//Above we implemented by opening file but the path was local only
-	String[] prohibWords = {"a","about","above","after","again","against","all","am","an","and","any","are","aren't","as","at","be","because","been","before","being","below","between","both","but","by","can't","cannot","could","couldn't","did","didn't","do","does","doesn't","doing","don't","down","during","each","few","for","from","further","had","hadn't","has","hasn't","have","haven't","having","he","he'd","he'll","he's","her","here","here's","hers","herself","him","himself","his","how","how's","i","i'd","i'll","i'm","i've","if","in","into","is","isn't","it","it's","its","itself","let's","me","more","most","mustn't","my","myself","no","nor","not","of","off","on","once","only","or","other","ought","our","ours","ourselves","out","over","own","same","shan't","she","she'd","she'll","she","should","shouldn't","so","some","such","than","that","that's","the","their","theirs","them","themselves","then","there","there's","these","they","they'd","they'll","they're","they've","this","those","through","to","too","under","until","up","very","was","wasn't","we","we'd","we'll","we're","we've","were","weren't","what","what's","when","when's","where","where's","which","while","who","who's","whom","why","why's","with","won't","would","wouldn't","you","you'd","you'll","you're","you've","your","yours","yourself","yourselves"};
+        List<String> prohibWords = new ArrayList<>();
+	//Used the Array
+	prohibWords.addAll(Arrays.asList("a","about","above","after","again","against","all","am","an","and","any","are","aren't","as","at","be","because","been","before","being","below","between","both","but","by","can't","cannot","could","couldn't","did","didn't","do","does","doesn't","doing","don't","down","during","each","few","for","from","further","had","hadn't","has","hasn't","have","haven't","having","he","he'd","he'll","he's","her","here","here's","hers","herself","him","himself","his","how","how's","i","i'd","i'll","i'm","i've","if","in","into","is","isn't","it","it's","its","itself","let's","me","more","most","mustn't","my","myself","no","nor","not","of","off","on","once","only","or","other","ought","our","ours","ourselves","out","over","own","same","shan't","she","she'd","she'll","she","should","shouldn't","so","some","such","than","that","that's","the","their","theirs","them","themselves","then","there","there's","these","they","they'd","they'll","they're","they've","this","those","through","to","too","under","until","up","very","was","wasn't","we","we'd","we'll","we're","we've","were","weren't","what","what's","when","when's","where","where's","which","while","who","who's","whom","why","why's","with","won't","would","wouldn't","you","you'd","you'll","you're","you've","your","yours","yourself","yourselves"));
         /*
         scanProhib.useDelimiter("\n");
         while (scanProhib.hasNext())
@@ -77,14 +75,14 @@ public class Main {
         words = words.replaceAll("--", "");
 	//words = words.replaceAll("-", ""); for night-cap to nightcap, sample output and the dash in there
 
-        for (int i=0; i<prohibWords.length; i++)
+        for (int i=0; i<prohibWords.size(); i++)
         {	
-            words = words.replaceAll("\\s*\\b"+prohibWords[i]+"\\b\\s*", " ");
-            //prohibWords.set(i, prohibWords[i].replaceAll("'", ""));
+            words = words.replaceAll("\\s*\\b"+prohibWords.get(i)+"\\b\\s*", " ");
+            prohibWords.set(i, prohibWords.get(i).replaceAll("'", ""));
         }
-        for (int i=0; i<prohibWords.length; i++)
+        for (int i=0; i<prohibWords.size(); i++)
         {	
-            words = words.replaceAll("\\s*\\b"+prohibWords[i]+"\\b\\s*", " ");
+            words = words.replaceAll("\\s*\\b"+prohibWords.get(i)+"\\b\\s*", " ");
         }
         
 	
@@ -94,7 +92,7 @@ public class Main {
 	for(int i = 0; i < wordsList.length-1; i++){
 		sentenceList[i] = wordsList[i];
 	}
-	System.out.println(sentenceList.length);
+	
         PorterStemmer ps = new PorterStemmer();
        
 	
